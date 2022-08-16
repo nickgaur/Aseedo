b37e10e7a90c7e881cf7775d057a760d18ae1d62
nickgaur <nikhilgaur3@rocketmail.com>
2022-08-16 17:20:26 +0530

    database config

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const bodyParser = require('body-parser');
 const homeRoutes = require('./routes/home');
 const businessOwnerRoutes = require('./routes/signUp/businessOwner');
 
-const dbUrl = 'mongodb://localhost:27017/aseedo';     // For development
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/aseedo'
+// const dbUrl = 'mongodb://localhost:27017/aseedo';     // For development
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/aseedo'
 
 
 main().catch((err) => console.log(err));
